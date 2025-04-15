66e80d6e5e99352add2a580dd4db984c332e7df6
rayanmalike <153559741+rayanmalike@users.noreply.github.com>
2025-04-15 16:13:16 -0700

    Update manager.py other methods

--- src/models/manager.py
+++ src/models/manager.py
@@ -14,16 +14,40 @@ class Manager(User):
         return new_task
 
     def update_task():
-        pass
+        if task.id not in self.tasks:
+            raise ValueError("Task not found.")
+        task = [self.tasks[task.id]
+        if title is not None:
+            task.set_title(title)
+        if description is not None:
+            task.set_description(description)
+        if due_date is not None:
+            task.set_due_date(due_date)
+        if priority is not None:
+            task.set_priority(priority)
+        if status is not None:
+            task.set_status(status)
+        if assigned_user is not None:
+            task.set_assigned_user(assigned_user)
+        return task
 
     def delete_task():
-        pass
+        if task_id in self.tasks:
+            del sel.tasks[task.id]
+        else:
+            raise ValueError("Task not found.")
 
     def assign_task():
-        pass
+        if task_id not in self.tasks:
+            raise ValueError("Task not found.")
+        task = self.tasks[task.id]
+        task.set_assigned_user(user)
+        return task
 
     def track_task_status():
-        pass
+        if task_id not in self.tasks:
+            raise ValueError("Task not found.")
+        return self.tasks[task_id].get_status()
 
     def create_project():
         pass
