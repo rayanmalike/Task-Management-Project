6a843569d50ed8dea638356b1b19ae10ff90394f
tristannn0 <tristanenriquezschool@gmail.com>
2025-04-16 10:12:48 -0700

    Employee & Project Controller
    Finished Employee, starting Project Controller

--- src/models/employee.py
+++ src/models/employee.py
@@ -5,28 +5,28 @@ class Employee(User):
         super().__init__(id, name, email, role)
         self.assigned_tasks = []
 
-    def get_task():
-        pass
+    def get_task(self, input):
+        if input <= 0 or input > (len(self.assigned_tasks) - 1) :
+            raise ValueError('Not a valid input.')
+        elif 1 <= input <=  (len(self.assigned_tasks) - 1) :
+            return self.assigned_tasks[input]
 
-    def get_task_description():
-        pass
+    def get_task_description(self, task):
+        return task.get_description()
 
-    def submit_task():
-        pass
+    #Removed submit_task, handled by task_controller object
 
-    def get_task_due_date():
-        pass
+    def get_task_due_date(self, task):
+        return task.get_due_date()
 
-    def get_task_priority():
-        pass
+    def get_task_priority(self, task):
+        return task.get_priority()
 
-    def get_project_description():
-        pass
+    def get_project_description(self, project):
+        return project.get_description()
 
-    def get_project_due_date():
-        pass
+    def get_project_end_date(self, project):
+        return project.get_end_date()
 
-    def get_task_priority():
-        pass
 
     
