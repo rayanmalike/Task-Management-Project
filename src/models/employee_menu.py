29e593c017b2dff12b441ddb8be05e496ee9eba2
Annn <anminh19999@gmail.com>
2025-04-19 22:34:44 -0700

    Create employee_menu.py

---
+++ src/models/employee_menu.py
@@ -0,0 +1,10 @@
+
+def show_employee_menu(username): # ->"username" parameter used to identify whether it's a boss, manager, or employee based on their "role".
+        print("""
+====== Employee DASHBOARD ======
+1. ### -> NEED TO BE FILLED
+2. ### -> NEED TO BE FILLED
+3. ### -> NEED TO BE FILLED 
+4. Logout -> NEED TO BE FILLED 
+""")
+### -> Need to be filled in menu options for Employee.
